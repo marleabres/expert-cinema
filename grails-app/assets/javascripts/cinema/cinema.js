e03baecc290cf26bb15f7de2ece7d5ee1527be94
Martín <marleabres@gmail.com>
2016-11-30 06:30:17 -0300

    First commit

---
+++ grails-app/assets/javascripts/cinema/cinema.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /cinema/core/cinema.core
+//= require /cinema/index/cinema.index
+
+angular.module("cinema", [
+    "cinema.core",
+    "cinema.index"
+]);
